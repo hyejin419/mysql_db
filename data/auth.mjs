7687d553b99a311dde672a39483c7fbd50e2887e
Hyejin Kim <gpwls041932@gmail.com>
2025-09-03 00:10:47 +0900

    mysql 실습 코드 업로드

---
+++ data/auth.mjs
@@ -0,0 +1,30 @@
+import { db } from "../db/database.mjs";
+
+export async function createUser(user) {
+    const { userid, password, name, email, url } = user;
+    return db
+        .execute(
+            "insert into users (userid, password, name, email, url) values (?, ?, ?, ?, ?)",
+            [userid, password, name, email, url]
+        )
+        .then((result) => result[0].insertId);
+}
+
+export async function login(userid, password) {
+    const user = users.find(
+        (user) => user.userid === userid && user.password === password
+    );
+    return user;
+}
+
+export async function findByUserid(userid) {
+    return db
+        .execute("select * from users where userid=?", [userid])
+        .then((result) => result[0][0]);
+}
+
+export async function findByid(idx) {
+    return db
+        .execute("select * from users where idx=?", [idx])
+        .then((result) => result[0][0]);
+}
---
+++ data/post.mjs
@@ -0,0 +1,50 @@
+import { db } from "../db/database.mjs";
+
+const SELECT_JOIN =
+    "select u.userid, u.name, u.url, p.idx, p.useridx, p.text, p.createAt from users as u join posts as p on u.idx = p.useridx";
+const ORDER_DESC = "order by p.createAt desc";
+
+// 모든 포스트를 리턴
+export async function getAll() {
+    return db
+        .execute(`${SELECT_JOIN} ${ORDER_DESC}`)
+        .then((result) => result[0]);
+}
+
+// 사용자 아이디(userid)에 대한 포스트를 리턴
+// 조건을 만족하는 모든 요소를 배열로 리턴
+export async function getAllByUserid(userid) {
+    return db
+        .execute(`${SELECT_JOIN} where u.userid=? ${ORDER_DESC}`, [userid])
+        .then((result) => result[0]);
+}
+
+// 글 번호(id)에 대한 포스트를 리턴
+// 조건을 만족하는 첫 번째 요소 하나를 리턴
+export async function getById(idx) {
+    return db
+        .execute(`${SELECT_JOIN} where p.idx=?`, [idx])
+        .then((result) => result[0][0]);
+}
+
+// 포스트 작성
+export async function create(text, useridx) {
+    return db
+        .execute("insert into posts (useridx, text) values (?, ?)", [
+            useridx,
+            text,
+        ])
+        .then((result) => getById(result[0].insertId));
+}
+
+// 포스트 변경
+export async function update(idx, text) {
+    return db
+        .execute("update posts set text=? where idx=?", [text, idx])
+        .then(() => getById(idx));
+}
+
+// 포스트 삭제
+export async function remove(idx) {
+    return db.execute("delete from posts where idx=?", [idx]);
+}
